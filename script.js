3228894b9e34da7eb739f7141c82a973b8f64cf8
Kaio Campos <kaiocamposti@gmail.com>
2019-08-06 18:59:56 -0300

    comandos basicos e um joguinho para treinar

---
+++ script.js
@@ -0,0 +1,19 @@
+function apertouMouse() {
+    console.log("APERTOU O MOUSE!");
+}
+
+function soltouMouse() {
+    console.log("SOLTOU O MOUSE!!");
+}
+
+function passouMouse() {
+    console.log("Mouse té encima!");
+}
+
+function mousesaiu() {
+    console.log("Mouse saiu de cima!");
+}
+
+function moveumouse() {
+    console.log("Movou o mouse!");
+}
